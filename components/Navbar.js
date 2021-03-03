755bc0a484bfa806ee669d1ce9679e64acdc5d39
cobraaz <anujbansal077@gmail.com>
2021-03-03 23:49:38 +0530

    Added Layout Page & Navbar components

---
+++ components/Navbar.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const Navbar = () => {
+  const router = useRouter();
+  const isActive = (route) => (route === router.pathname ? "active" : "");
+  return (
+    <nav>
+      <div className="nav-wrapper #1565c0 blue darken-3">
+        <Link href="/">
+          <a className="brand-logo left">Logo</a>
+        </Link>
+        <ul id="nav-mobile" className="right ">
+          <li className={isActive("/login")}>
+            <Link href="/login">
+              <a>login</a>
+            </Link>
+          </li>
+
+          <li className={isActive("/signup")}>
+            <Link href="/signup">
+              <a>signup</a>
+            </Link>
+          </li>
+          <li className={isActive("/create")}>
+            <Link href="/create">
+              <a>create</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
